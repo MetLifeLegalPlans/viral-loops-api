8cb33f8806af0f28baab7cadc8fa976b23c5b7d7
Mark Rawls <markrawls96@gmail.com>
2019-06-07 15:29:04 -0400

    Update setup.py to use README as long description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+readme_content = ''
+
+with open('README.md', 'r') as f:
+  readme_content = f.read()
+
 setup(
   name='viral-loops-api',
-  version='0.1',
+  version='0.3',
   description='A simplistic API wrapper for viral-loops',
+  long_description=readme_content,
+  long_description_content_type='text/markdown',
   url='https://github.com/bequest/viral-loops-api',
   author='Bequest Inc.',
   author_email='oss@willing.com',
